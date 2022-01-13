bigbiff/android_device_google_redfin

redfin: add twrp target and move install_keyring to encrypted property

AndroidProducts.mk
@@ -16,6 +16,7 @@
 
 PRODUCT_MAKEFILES := \
     $(LOCAL_DIR)/aosp_redfin.mk \
+    $(LOCAL_DIR)/twrp_redfin.mk 
 
 COMMON_LUNCH_CHOICES := \
     aosp_redfin-userdebug \

JoshuaDoes/twrp_device_google_bramble

Change-Id: Ia840304dd26d88101704a9cdba995bcbe01dbc74

AndroidProducts.mk
@@ -16,6 +16,7 @@
 
 PRODUCT_MAKEFILES := \
     $(LOCAL_DIR)/aosp_bramble.mk \
+    $(LOCAL_DIR)/twrp_bramble.mk
 
 COMMON_LUNCH_CHOICES := \
     aosp_bramble-userdebug \